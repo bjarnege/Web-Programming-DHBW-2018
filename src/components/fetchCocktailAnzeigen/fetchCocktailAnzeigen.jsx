8824cf9a1f0a94b411212cb20155e1db4cf386c4
bjarnege <24606871+bjarnege@users.noreply.github.com>
2020-02-27 19:27:29 +0100

    Cocktailanzeige designt.

--- src/components/fetchCocktailAnzeigen/fetchCocktailAnzeigen.jsx
+++ src/components/fetchCocktailAnzeigen/fetchCocktailAnzeigen.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import "./fetchCocktail_Anzeigen.css"
+import Button from "@material-ui/core/Button"
 
 const fetchCocktailAnzeigen = (props) => {
     const calculatePercentage = (props) => {
@@ -9,28 +11,51 @@ const fetchCocktailAnzeigen = (props) => {
         }
     };
 
-    return (<div className="CocktailListElementWrapper">
+    return (
+
+        <div className="CocktailListElementWrapper">
             <Link to={{
                 pathname: '/Rezept',
                 state: {
                     name: props.name,
                     cocktails: props.cocktails,
-                    state: props.state
+                    state: props.state,
+                    ingredients: props.cocktails.ingredients
                 }
             }}
             >
-                <div className="CocktailListElement">
+                <div  className="CocktailListElement">
+
                     <div className="CocktailListElementDescription">
-                        <h2>{props.name}</h2>
-                        <h3>{calculatePercentage(props)}% of ingredients in stock</h3>
-                    </div>
-                    <div className="CocktailImage">
-                        <img className="img" src={require(`../../Images/${props.cocktails[props.name][2]}`)}/>
+                        <div>
+                            <h2 align="left">{props.name}</h2>
+                            <h4 align="left">{calculatePercentage(props)}% der Zutaten verfügbar.</h4>
+
+
+                        </div>
+                        <div className="CocktailImage">
+                            <img className="img" src={require(`../../Images/${props.cocktails[props.name][2]}`)}/>
+                        </div>
+                        <div  className="ShowMoreButton">
+                            <Button className="" variant="contained" color="primary">
+                                Rezept ansehen
+                            </Button>
+                        </div>
+                        <hr color="black" width="100%"/>
+
                     </div>
 
+
+
+
                 </div>
+
+
+
             </Link>
         </div>
+
+
     );
 };
 
