56687b5e6516ba11e92776400609264f463242d3
bjarnege <24606871+bjarnege@users.noreply.github.com>
2020-02-27 14:25:49 +0100

    Rezeptseite erstellt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import CocktailsFinden from "./components/cocktailsFinden/cocktailFinden";
 import CocktailsAnzeigen from "./components/cocktailsAnzeigen/cocktailsAnzeigen";
-import Rezepte from "./components/Rezepte/rezepte";
+import Rezept from "./components/Rezepte/Rezept";
 import './App.css';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
           {/* Routing initialisiert */}
           <Switch>
               <Route path="/cocktailsAnzeigen" component={CocktailsAnzeigen}/>
-              <Route path="/Rezepte" component={Rezepte}/>
+              <Route path="/Rezept" component={Rezept}/>
               <Route path="/" component={CocktailsFinden}/>
               <Redirect from="/" to="/"/>
               <Route render={()=><h1>Diese Seite konnte nicht gefunden werden!</h1>}/>
---
+++ src/components/Rezepte/Rezept.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from "react"
+import "./Rezept.css"
+
+class Rezept extends Component {
+    missing = (einkaufen) => {
+        if (einkaufen.length > 0) {
+            return (
+                <div className="MissingIngredients">
+                    <h2>Benötigte Zutaten</h2>
+                    {
+                        Object.keys(einkaufen).map(function (key, index) {
+                            return <p key={key}>{einkaufen[index]}</p>
+                        }, this)
+                    }
+                </div>
+            )
+        }
+    };
+
+    render() {
+
+        const props = this.props.location.state;
+        const gesamt = props.cocktails[props.name][0];
+        const einkaufen = props.cocktails[props.name][0].filter(x => !props.state[props.name].includes(x));
+
+        return (
+            <div className="Frame">
+                <h1>{props.name}</h1>
+                <div className="RecipeCocktailImage">
+                    <img className="RecipeImg" src={require(`../../Images/${props.cocktails[props.name][2]}`)}/>
+                </div>
+                <div className="RecipeInfoContainer">
+                    <div className="NeededIngredients">
+                        <h2>Vorhandene Zutaten</h2>
+                        {
+                            Object.keys(gesamt).map(function (key, index) {
+                                return <p key={key}>{gesamt[key]}</p>
+                            }, this)
+                        }
+                    </div>
+                    {this.missing(einkaufen)}
+                    <div className="Preparation">
+                        <h2>Zubereitung</h2>
+                        <p>{props.cocktails[props.name][1]}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Rezept;
